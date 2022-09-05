9dc4fcefbaafa0e61b1b54a3966bf0c057fd7561
rupaak <rupakt525@gmail.com>
2022-09-05 12:48:35 +0545

    first commit

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import "express-async-errors";
 import { indexAdminRouter } from "./routes/v1/admin";
-import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 
@@ -12,5 +11,4 @@ app.use(cors());
 
 app.use(indexAdminRouter);
 
-app.use(errorHandler);
 export { app };
